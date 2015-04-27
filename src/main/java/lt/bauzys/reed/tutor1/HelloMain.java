9cddb32abf589adac3463f393041f5321f3ca7bb
Remis B <bauzys@gmail.com>
2015-04-27 19:54:04 +0300

    Initial commit

---
+++ src/main/java/lt/bauzys/reed/tutor1/HelloMain.java
@@ -0,0 +1,10 @@
+package lt.bauzys.reed.tutor1;
+
+/**
+ * Created by Remis on 27/04/15.
+ */
+public class HelloMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
