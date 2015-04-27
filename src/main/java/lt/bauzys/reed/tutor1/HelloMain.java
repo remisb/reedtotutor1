69c1905fd2b4941134b0b0fd06e7335955695d98
Remis B <bauzys@gmail.com>
2015-04-27 19:57:24 +0300

    We have updates hello message to the better one. How

--- src/main/java/lt/bauzys/reed/tutor1/HelloMain.java
+++ src/main/java/lt/bauzys/reed/tutor1/HelloMain.java
@@ -5,6 +5,6 @@ package lt.bauzys.reed.tutor1;
  */
 public class HelloMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hello world version 2!!!");
     }
 }
